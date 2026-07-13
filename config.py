09e340be5e0908fac77b434f241fa4cefde694a8
AngelHernandez <angelhdezcrdenas@gmail.com>
2026-07-13 16:20:39 -0600

    update webhook URL and fix Apps Script row targeting

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ MAX_JUECES_POR_CHECKPOINT = 2
 
 # --- Integración con Google Sheets Webhook ---
 # Debes llenar esta variable con la URL que obtengas al implementar tu script
-GOOGLE_APPS_SCRIPT_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxPKNktzvNdrHW1I8U9QNXakuDvNpRXZk56BnlJ_BC6tyfS2_9J82o5SbREz9Lnn5I-/exec"
+GOOGLE_APPS_SCRIPT_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbwQfSzqKW4LVcPaMyZMQOw1EJMdjcFjAZrshJ33VOZFEdBQD5-NJeCxw-v0FtERjR0L/exec"
 
 # --- Duración de la sesión ---
 DURACION = 24 * 60 * 60  # 24 horas en segundos
